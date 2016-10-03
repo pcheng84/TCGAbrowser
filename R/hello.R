88bb412bf68246be46ec645d4c674777c4ce0dd0
Phil Cheng <cheng.phil@gmail.com>
2016-10-03 17:16:46 +0200

    First commit

---
+++ R/hello.R
@@ -0,0 +1,15 @@
+#' Hello function
+#'
+#' Outputs your name and counts characters
+#'
+#' @param who character(1) holds name of person
+#'
+#' @return character(1) displays name and number of characters
+#'
+#' @examples
+#' hello("Martin")
+#'
+#' @export
+hello <- function(who) {
+  sprintf("Hello, %s, your name is %d characters", who, nchar(who))
+}
