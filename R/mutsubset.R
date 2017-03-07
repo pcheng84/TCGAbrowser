ba3f3b5addfc8a15382a5c8bce288629f010ec57
pcheng84 <phil.cheng@usz.ch>
2017-03-07 16:28:19 +0100

    Fixed mutsubset to only take the first match if multiple samples with mutation.

--- R/mutsubset.R
+++ R/mutsubset.R
@@ -20,7 +20,7 @@ mutsubset <- function(pat, rna, mut, gene) {
   #retrieves patients with mutation
   mutpat <- intersect(pat$bcr_patient_barcode, colnames(mut))
   setkey(pat, bcr_patient_barcode)
-  pat2 <- pat[mutpat]
+  pat2 <- pat[mutpat, ,mult = "first"]
 
   pat2[, gene := as.numeric(mut[gene, mutpat, with = FALSE])]
   pat2[, gene2 := factor(gene, levels = 1:0, labels =c("Mutated", "WT"))]
