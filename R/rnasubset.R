1f62d85ba3d42d4f46f2d793626e75c8303a4706
Patrick Turko <patrick.turko@gmail.com>
2019-02-01 13:54:57 +0100

    rnasubset() now returns entire original MAE object.

--- R/rnasubset.R
+++ R/rnasubset.R
@@ -1,6 +1,6 @@
 #' rnasubset function
 #'
-#' Subsets MultiAssayExperiment object to one gene of interest, and add expression level column to colData.
+#' Annotates MultiAssayExperiment object to indicate which samples have high, medium, or low expression of a gene of interest.
 #'
 #' @param mae MultiAssayExperiment object containing data from curatedTCGAdata. Must include RNASeq2GeneNorm assay.
 #' @param percent numeric(1) percentile of patients to compare 1-50
@@ -18,22 +18,32 @@ rnasubset <- function(mae, gene, percent) {
   #To do: test whether this is really a MAE object, and whether it has the appropriate assay.
   assay_num <- grep("RNASeq2GeneNorm", names(mae))
 
-  #Extract RNASeq data for desired gene, but keep it in MAE format. This will be edited and returned.
+  #Extract RNASeq data for desired gene, but keep it in MAE format.
   #To do: make sure that gene is a single character object (fail and exit if given multiple genes)
   pat.mae <- mae[gene, , assay_num]
 
-  #Get RNA data as a matrix for calculations
+  #Merge replicates (now taking minimum, could be mean or maximum), and ensure that all samples in assay are present in colData
+  pat.mae <- intersectColumns(pat.mae)
+  pat.mae <- mergeReplicates(pat.mae, simplify = min)
+
+  #Get RNA data as a data.frame for calculations
   #To do: make sure that at least 3 RNA values are given.
-  rna.mat <- assay(pat.mae)
+  rna.mat <- longFormat(pat.mae)
 
   #Find RNA expression values corresponding to top and bottom percentile
-  percents <- quantile(rna.mat, c(percent / 100, (1 - percent / 100)))
+  percents <- quantile(rna.mat$value, c(percent / 100, (1 - (percent / 100))))
 
-  #Mark samples with low, medium, and high values (in pat.mae colData) based on above percentiles
-  pat.mae$level <- "middle"
-  pat.mae$level[rna.mat <= percents[1]] <- "low"
-  pat.mae$level[rna.mat >= percents[2]] <- "high"
+  #Find which samples have low and high values based on above percentiles
+  low.samps <- rna.mat$primary[rna.mat$value <= percents[1]]
+  high.samps <- rna.mat$primary[rna.mat$value >= percents[2]]
+  med.samps <- rna.mat$primary[rna.mat$value > percents[1] & rna.mat$value < percents[2]]
 
-  #return MAE object, now subsetted to gene of interest, with new colData column indicating expression level
-  pat.mae
+  #Mark low, medium, and high expression values in original MAE colData. Mark samples NA if they have no RNA data.
+  mae$level <- NA
+  mae$level[mae$patientID %in% med.samps] <- "medium"
+  mae$level[mae$patientID %in% low.samps] <- "low"
+  mae$level[mae$patientID %in% high.samps] <- "high"
+
+  #return MAE object with new colData column indicating expression level
+  mae
 }
