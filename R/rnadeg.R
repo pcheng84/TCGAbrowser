7b7a476d6bf3f11be079f66d870a9f919023b931
Patrick Turko <patrick.turko@gmail.com>
2019-02-05 11:10:15 +0100

    rnadeg() now accepts MultiAssayExperiment with an expression level assay created by rnasubset()

--- R/rnadeg.R
+++ R/rnadeg.R
@@ -19,31 +19,25 @@
 #'
 rnadeg <- function(mae) {
 
-  #Find which assay contains RNA data (assay names have suffixes and prefixes for each cancer)
-  #To do: test whether this is really a MAE object, and whether it has the appropriate assay.
-  assay_num <- grep("RNASeq2GeneNorm", names(mae))
+  #Get expression levels (which were appended by function rnasubset()), ID those with high / low expression
+  level_assay_num <- grep("ExpressionLevel", names(mae))
+  levels <- mae[[level_assay_num]]
+  good_levels <- colnames(levels)[levels != "medium"]
 
-  #Extract RNASeq data for all genes, but keep it in MAE format.
-  #To do: make sure that gene is a single character object (fail and exit if given multiple genes)
-  pat.mae <- mae[, , assay_num]
-
-  #Merge replicates (now taking minimum, could be mean or maximum), and ensure that all samples in assay are present in colData
-  pat.mae <- intersectColumns(pat.mae)
-  pat.mae <- mergeReplicates(pat.mae, simplify = min)
-
-  #Convert counts to matrix, expression levels to vector
-  counts <- assay(pat.mae[, pat.mae$level != "medium", ])
-  level <- pat.mae$level[pat.mae$level != "medium"]
+  #Extract RNASeq data for all genes, subset out the "medium" expression group
+  rna_assay_num <- grep("RNASeq2GeneNorm", names(mae))
+  counts <- assay(mae[[rna_assay_num]])
+  counts <- counts[, good_levels]
 
   #Create DGEList
-  deg <- DGEList(counts = counts, genes = rownames(counts), group = level)
+  deg <- DGEList(counts = counts, genes = rownames(counts), group = levels[good_levels])
 
   #Only keeps genes with at least 1 count-per-million in at least half the samples
   isexpr <- rowSums(cpm(deg)>1) >= (ncol(deg)/2)
   deg <- deg[isexpr,]
 
   #Create model matrix
-  design <- model.matrix(~factor(level))
+  design <- model.matrix(~factor(levels[, good_levels]))
 
   #Limma - voom
   v2 <- voom(deg, design, plot = F)
