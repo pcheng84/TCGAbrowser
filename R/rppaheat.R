38f0bafc62dccbe2f0afd5bf8e063b8568054458
Patrick Turko <patrick.turko@gmail.com>
2019-02-21 12:05:18 +0100

    Rppaheat() now accepts MAE object. Plots all RPPA data, not just top 100. To change?

--- R/rppaheat.R
+++ R/rppaheat.R
@@ -10,6 +10,7 @@
 #' @import ComplexHeatmap
 #' @import circlize
 #' @importFrom edgeR cpm
+#' @importFrom TCGAutils TCGAbarcode
 #'
 #' @return ComplexHeatmap of top 100 significant differentially expressed genes
 #'
@@ -21,29 +22,55 @@
 #'
 #' @export
 #'
-rppaheat <- function(pat2, rppa, gene) {
-  setkey(pat2, name)
-  rppa.pat <- pat2[intersect(colnames(rppa), pat2[pat2$gene2 != "middle", name])]
+rppaheat <- function(mae, gene, num_genes) {
+  #Find which assays contain expression levels, RPPA data, and RNASeq data
+  exp_assay <- grep("Expression", names(mae))
+  rppa_assay <- grep("RPPA", names(mae))
+  rna_assay <- grep("RNASeq2GeneNorm", names(mae))
 
-  rppa.gene <- as.matrix(rppa[,rppa.pat$name, with=F])
-  rownames(rppa.gene) <- rppa$CE.REF
-  rppa.gene <- rppa.gene[complete.cases(rppa.gene),]
-  setkey(rppa.pat, gene2)
-  df <- data.frame(as.character(rppa.pat[!("middle"),gene2]),
-                   rppa.pat[!("middle"), exprs_rank])
+  #Get names of samples with high and low expression, convert them to common format
+  high <- TCGAbarcode(colnames(mae[[exp_assay]])[mae[[exp_assay]] == "high"])
+  low  <- TCGAbarcode(colnames(mae[[exp_assay]])[mae[[exp_assay]] == "low"])
+
+  #Find the patients in RPPA assay corresponding to above
+  overlap_high <- TCGAbarcode(colnames(mae[[rppa_assay]])) %in% high
+  overlap_high <- colnames(mae[[rppa_assay]])[overlap_high]
+
+  overlap_low <- TCGAbarcode(colnames(mae[[rppa_assay]])) %in% low
+  overlap_low <- colnames(mae[[rppa_assay]])[overlap_low]
+  all_overlaps <- c(overlap_high, overlap_low)
+
+  #Extract RPPA data, limit to samples ID'd above, convert to matrix, get complete cases, convert names to common format
+  counts <- mae[[rppa_assay]]
+  counts <- counts[, all_overlaps]
+  counts <- as.matrix(assay(counts))
+  counts <- counts[complete.cases(counts), ]
+  colnames(counts) <- TCGAbarcode(colnames(counts))
+
+  #Make heatmap annotation data frame, 2 columns, first is high/low, second is gene expression
+  df <- data.frame(c(rep("high", times = length(overlap_high)),
+                     rep("low" , times = length(overlap_low))),
+                   c(assay(mae[gene, TCGAbarcode(overlap_high), rna_assay]),
+                     assay(mae[gene, TCGAbarcode(overlap_low ), rna_assay])))
   colnames(df) <- c(paste0(gene, "_group"), paste0(gene, "_expression"))
+
+  #make colors, name them high/low
   cellcol <- c("#ca0020", "#0571b0")
-  names(cellcol) <- c(levels(rppa.pat$gene2)[1], levels(rppa.pat$gene2)[2])
+  names(cellcol) <- c("high", "low")
   col1 <- list(Cell = cellcol,
-               expression = colorRamp2(range(rppa.pat$exprs_rank), c("white", "purple")))
+               expression = colorRamp2(range(df[, 2]), c("white", "purple")))
   names(col1) <- c(paste0(gene, "_group"), paste0(gene, "_expression"))
+
+  #Complete heatmap annotation object
   top_ha <- HeatmapAnnotation(df = df, col = col1)
 
-  x <- Heatmap(rppa.gene, top_annotation = top_ha, name = "color scale",
+  #Draw and decorate heatmap
+  x <- Heatmap(counts, top_annotation = top_ha, name = "color scale",
                show_column_names = T,
                row_names_gp = gpar(fontsize = 8),
                column_names_gp = gpar(fontsize = 6),
                column_dend_reorder = as.numeric(df[, 1]))
+
   draw(x)
   for(an in colnames(df)) {
     decorate_annotation(an, {
